354b718707edfda89a5bcacc273a0367319f9789
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-10-14 20:48:18 +0900

    Add The true basics3.R

---
+++ 1.Basics/The true basics3.R
@@ -0,0 +1,36 @@
+# Little arithmetics with R
+# 
+# In its most basic form R can be used as a simple calculator. 
+# Consider the following arithmetic operators:
+#   
+#   Addition: +
+#   Subtraction: -
+#   Multiplication: *
+#   Division: /
+#   Exponentiation: ^
+#   Modulo: %%
+#   The last two might need some explaining:
+#   
+# The ^ operator raises the number to its left to the power of the number 
+# to its right: for example 3^2 equals 9.
+# The modulo returns the remainder of the division of the number 
+# to the left by the number on its right, 
+# for example 5 modulo 3 or 5 %% 3 equals 2.
+
+# Addition
+5 + 5
+
+# Subtraction
+5 - 5
+
+# Multiplication
+3 * 5
+
+# Division
+(5 + 5) / 2
+
+# Exponentiation
+2^5
+
+# Modulo
+28%%6
