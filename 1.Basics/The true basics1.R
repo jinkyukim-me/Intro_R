fead16edf1cc4a368b25b7e0ef7fad1b56a5df98
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-10-14 20:47:22 +0900

    Add The true basics1.R

---
+++ 1.Basics/The true basics1.R
@@ -0,0 +1,20 @@
+# How it works
+# 
+# In the editor on the right you should type R code to solve the exercises. 
+# When you hit the 'Submit Answer' button, 
+# every line of code in the script is interpreted 
+# and executed by R and you get a message that indicates 
+# whether or not your code was correct. 
+# 
+# The output of your submission is shown in the console in the lower right corner.
+# 
+# You can also execute R commands straight in the console. 
+# This is a good way to experiment with R code: 
+# When you type in the console, 
+# your submission will not be checked for correctness!
+
+
+3 + 4
+
+6 + 12
+
