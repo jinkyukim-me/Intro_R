2f5d4d5b97edfb9a1ab0404c8e3e65d092be1f00
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-10-14 20:48:27 +0900

    Add The true basics4.R

---
+++ 1.Basics/The true basics4.R
@@ -0,0 +1,16 @@
+# Variable assignment
+# 
+# A basic concept in R programming is the variable. 
+# It allows you to store a value or an object in R. 
+# You can then later use this variable's name 
+# to easily access the value or the object that is stored within this variable. 
+# You use <- to assign a variable:
+# 
+# my_variable <- 4
+
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
