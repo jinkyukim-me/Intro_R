c30a0ffbb17f0e3358c713a3b856666b87625eb3
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-10-14 20:47:29 +0900

    Add The true basics2.R

---
+++ 1.Basics/The true basics2.R
@@ -0,0 +1,18 @@
+# Documenting your code
+# 
+# Adding comments to your code is extremely important 
+# to make sure that you and others can understand what your code is about. 
+# 
+# R makes use of the # sign to add comments, just like Twitter!
+# 
+# It's important to note that comments are not run as R code, 
+# so they will not influence your result. 
+# For example, Calculate 3 + 4 in the editor on the right is a comment 
+# and is ignored during execution.
+
+
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+6 + 12
