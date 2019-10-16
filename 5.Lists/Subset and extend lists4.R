ba516bcdd54b7c32efa3be223252d1a6f02dfad8
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-10-16 10:39:10 +0900

    Add Subset and extend lists4.R

---
+++ 5.Lists/Subset and extend lists4.R
@@ -0,0 +1,12 @@
+# Extending a list (1)
+
+
+
+# Add the release year to shining_list
+shining_list$year <- 1980
+
+# Add the director to shining_list
+shining_list$director <- "Stanley Kubrick"
+
+# Inspect the structure of shining_list
+str(shining_list)
