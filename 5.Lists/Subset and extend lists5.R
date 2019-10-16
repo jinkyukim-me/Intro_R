87057263dd051449af7fc2949e11c32ad00abbca
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-10-16 10:39:17 +0900

    Add Subset and extend lists5.R

---
+++ 5.Lists/Subset and extend lists5.R
@@ -0,0 +1,8 @@
+# Extending a list (2)
+
+
+# Add both the year and director to shining_list: shining_list_ext
+shining_list_ext <- c(shining_list, year = 1980, director = "Stanley Kubrick")
+
+# Have a look at the structure of shining_list_ext
+str(shining_list_ext)
